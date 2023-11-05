d1ea872c3c613c6ac88b28c98d4debd7c6316e41
Anya Lin <hukk1996@gmail.com>
2023-11-05 12:05:20 +0800

    20231105

--- htdocs/luci-static/resources/view/dnsproxy.js
+++ htdocs/luci-static/resources/view/dnsproxy.js
@@ -60,30 +60,30 @@ return view.extend({
 		var isRunning = res[0],
 			hosts = res[1];
 
-		var m, s, o;
+		var m, s, o, ss, so;
 
 		m = new form.Map('dnsproxy', _('DNS Proxy'));
 
 		s = m.section(form.NamedSection, '_status');
-		s.anonymous = true;
 		s.render = function (section_id) {
 			return E('div', { class: 'cbi-section' }, [
 				E('div', { id: 'service_status' }, _('Collecting data ...'))
 			]);
 		};
 
-		s = m.section(form.NamedSection, 'global', 'dnsproxy', _('Main Settings'));
-		s.anonymous = true;
+		s = m.section(form.NamedSection, 'global', 'dnsproxy');
 
-		o = s.option(form.Flag, 'enabled', _('Enable'));
+		s.tab('main', _('Main'));
+
+		o = s.taboption('main', form.Flag, 'enabled', _('Enable'));
 		o.default = o.disabled;
 
-		o = s.option(form.Flag, 'verbose', _('Verbose'));
+		o = s.taboption('main', form.Flag, 'verbose', _('Verbose'));
 
-		o = s.option(form.Value, 'log_file', _('Log file path'));
+		o = s.taboption('main', form.Value, 'log_file', _('Log file path'));
 		o.datatype = 'file';
 
-		o = s.option(form.DynamicList, 'listen_addr', _('Listen address'));
+		o = s.taboption('main', form.DynamicList, 'listen_addr', _('Listen address'));
 		o.datatype = "list(ipaddr(1))";
 		o.value('127.0.0.1');
 		o.value('::1');
@@ -105,94 +105,104 @@ return view.extend({
 			o.value(ipv6, ip6addrs[ipv6] ? '%s (%s)'.format(ipv6, ip6addrs[ipv6]) : ipv6);
 		});
 
-		o = s.option(form.DynamicList, 'listen_port', _('Listen ports'));
+		o = s.taboption('main', form.DynamicList, 'listen_port', _('Listen ports'));
 		o.datatype = "list(and(port, min(1)))";
 		o.default = '5353';
 		o.rmempty = false;
 
-		o = s.option(form.Flag, 'ipv6_disabled', _('Disable IPv6'));
+		o = s.taboption('main', form.Flag, 'ipv6_disabled', _('Disable IPv6'));
 
-		o = s.option(form.Flag, 'refuse_any', _('Refuse <code>ANY</code> requests'));
+		o = s.taboption('main', form.Flag, 'refuse_any', _('Refuse <code>ANY</code> requests'));
 
-		o = s.option(form.Flag, 'insecure', _('Disable secure TLS cert validation'));
+		o = s.taboption('main', form.Flag, 'insecure', _('Disable secure TLS cert validation'));
 
-		o = s.option(form.Flag, 'http3', _('DoH uses H3 first'));
+		o = s.taboption('main', form.Flag, 'http3', _('DoH uses H3 first'));
 
-		o = s.option(form.Value, 'timeout', _('Timeout for queries to remote upstream (default: 10s)'));
+		o = s.taboption('main', form.Value, 'timeout', _('Timeout for queries to remote upstream (default: 10s)'));
 		o.datatype = 'string';
 
-		o = s.option(form.Value, 'rate_limit', _('Ratelimit (requests per second)'));
+		o = s.taboption('main', form.Value, 'rate_limit', _('Ratelimit (requests per second)'));
 		o.datatype = "and(uinteger, min(1))";
 
-		o = s.option(form.Value, 'udp_buf_size', _('Size of the UDP buffer in bytes. Set 0 use the system default'));
+		o = s.taboption('main', form.Value, 'udp_buf_size', _('Size of the UDP buffer in bytes. Set 0 use the system default'));
 		o.datatype = 'uinteger';
 
-		o = s.option(form.Flag, 'all_servers', _('Parallel queries all upstream'));
+		o = s.taboption('main', form.Flag, 'all_servers', _('Parallel queries all upstream'));
 
-		o = s.option(form.Flag, 'fastest_addr', _('Respond to A or AAAA requests only with the fastest IP address'));
+		o = s.taboption('main', form.Flag, 'fastest_addr', _('Respond to A or AAAA requests only with the fastest IP address'));
 		o.depends('all_servers', '1');
 
-		s = m.section(form.NamedSection, 'cache', 'dnsproxy', _('Cache Settings'));
-		s.anonymous = true;
+		s.tab('cache', _('Cache'));
 
-		o = s.option(form.Flag, 'enabled', _('Enable Cache'));
+		o = s.taboption('cache', form.SectionValue, '_cache', form.NamedSection, 'cache', 'homeproxy');
+		ss = o.subsection;
 
-		o = s.option(form.Flag, 'cache_optimistic', _('Optimistic Cache'));
-		o.retain = true;
-		o.depends('enabled', '1');
+		so = ss.option(form.Flag, 'enabled', _('Enable Cache'));
 
-		o = s.option(form.Value, 'size', _('Cache size (in bytes)'));
-		o.datatype = "and(uinteger, min(512))";
-		o.default = '65535';
-		o.retain = true;
-		o.depends('enabled', '1');
+		so = ss.option(form.Flag, 'cache_optimistic', _('Optimistic Cache'));
+		so.retain = true;
+		so.depends('enabled', '1');
 
-		o = s.option(form.Value, 'min_ttl', _('Min TTL value for DNS entries, in seconds'));
-		o.datatype = "and(uinteger, range(1,3600))";
-		o.retain = true;
-		o.depends('enabled', '1');
+		so = ss.option(form.Value, 'size', _('Cache size (in bytes)'));
+		so.datatype = "and(uinteger, min(512))";
+		so.default = '65535';
+		so.retain = true;
+		so.depends('enabled', '1');
 
-		o = s.option(form.Value, 'max_ttl', _('Max TTL value for DNS entries, in seconds'));
-		o.datatype = "and(uinteger, min(60))";
-		o.retain = true;
-		o.depends('enabled', '1');
+		so = ss.option(form.Value, 'min_ttl', _('Min TTL value for DNS entries, in seconds'));
+		so.datatype = "and(uinteger, range(1,3600))";
+		so.retain = true;
+		so.depends('enabled', '1');
 
-		s = m.section(form.NamedSection, 'dns64', 'dnsproxy', _('DNS64 Settings'));
-		s.anonymous = true;
+		so = ss.option(form.Value, 'max_ttl', _('Max TTL value for DNS entries, in seconds'));
+		so.datatype = "and(uinteger, min(60))";
+		so.retain = true;
+		so.depends('enabled', '1');
 
-		o = s.option(form.Flag, 'enabled', _('Enable DNS64'));
+		s.tab('dns64', _('DNS64'));
 
-		o = s.option(form.Value, 'dns64_prefix', _('DNS64 Prefix'));
-		o.datatype = "ip6addr(1)";
-		o.default = '64:ff9b::';
-		o.retain = true;
-		o.depends('enabled', '1');
+		o = s.taboption('dns64', form.SectionValue, '_dns64', form.NamedSection, 'dns64', 'homeproxy');
+		ss = o.subsection;
 
-		s = m.section(form.NamedSection, 'edns', 'dnsproxy', _('EDNS Settings'));
-		s.anonymous = true;
+		so = ss.option(form.Flag, 'enabled', _('Enable DNS64'));
 
-		o = s.option(form.Flag, 'enabled', _('Enable EDNS'));
+		so = ss.option(form.Value, 'dns64_prefix', _('DNS64 Prefix'));
+		so.datatype = "ip6addr(1)";
+		so.default = '64:ff9b::';
+		so.retain = true;
+		so.depends('enabled', '1');
 
-		o = s.option(form.Value, 'edns_addr', _('EDNS Client Address'));
-		o.datatype = "ipaddr(1)";
-		o.retain = true;
-		o.depends('enabled', '1');
+		s.tab('edns', _('EDNS'));
 
-		s = m.section(form.NamedSection, 'bogus_nxdomain', 'dnsproxy', _('Bogus-NXDOMAIN'));
-		s.anonymous = true;
+		o = s.taboption('edns', form.SectionValue, '_edns', form.NamedSection, 'edns', 'homeproxy');
+		ss = o.subsection;
 
-		o = s.option(form.DynamicList, 'ip_addr', _('Convert matching single IP responses to NXDOMAIN'));
-		o.datatype = "list(ipaddr)";
+		so = ss.option(form.Flag, 'enabled', _('Enable EDNS'));
 
-		s = m.section(form.NamedSection, 'servers', 'dnsproxy', _('Upstreams'));
-		s.anonymous = true;
+		so = ss.option(form.Value, 'edns_addr', _('EDNS Client Address'));
+		so.datatype = "ipaddr(1)";
+		so.retain = true;
+		so.depends('enabled', '1');
 
-		o = s.option(form.DynamicList, 'bootstrap', _('Bootstrap DNS Server'));
+		s.tab('bogus_nxdomain', _('Bogus-NXDOMAIN'));
 
-		o = s.option(form.DynamicList, 'upstream', _('Upstream DNS Server'));
-		o.rmempty = false;
+		o = s.taboption('bogus_nxdomain', form.SectionValue, '_bogus_nxdomain', form.NamedSection, 'bogus_nxdomain', 'homeproxy');
+		ss = o.subsection;
 
-		o = s.option(form.DynamicList, 'fallback', _('Fallback DNS Server'));
+		so = ss.option(form.DynamicList, 'ip_addr', _('Convert matching single IP responses to NXDOMAIN'));
+		so.datatype = "list(ipaddr)";
+
+		s.tab('servers', _('Upstreams'));
+
+		o = s.taboption('servers', form.SectionValue, '_servers', form.NamedSection, 'servers', 'homeproxy');
+		ss = o.subsection;
+
+		so = ss.option(form.DynamicList, 'bootstrap', _('Bootstrap DNS Server'));
+
+		so = ss.option(form.DynamicList, 'upstream', _('Upstream DNS Server'));
+		so.rmempty = false;
+
+		so = ss.option(form.DynamicList, 'fallback', _('Fallback DNS Server'));
 
 		return m.render()
 		.then(L.bind(function(m, nodes) {
